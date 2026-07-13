6083bb8605f989f03b2a5f747b6a7c7558190078
ezTxmMC <tom.handrick@vesperia.net>
2026-07-13 15:50:17 +0200

    formatted codebase

--- astro.config.ts
+++ astro.config.ts
@@ -1,22 +1,22 @@
 import { defineConfig } from 'astro/config';
-import tailwindcss from "@tailwindcss/vite";
-import node from "@astrojs/node";
-import icon from "astro-icon";
+import tailwindcss from '@tailwindcss/vite';
+import node from '@astrojs/node';
+import icon from 'astro-icon';
 
 export default defineConfig({
-  integrations: [icon()],
-  output: "server",
+    integrations: [icon()],
+    output: 'server',
 
-  adapter: node({
-    mode: "standalone",
-  }),
+    adapter: node({
+        mode: 'standalone',
+    }),
 
-  vite: {
-    plugins: [tailwindcss()],
-    resolve: {
-      alias: {
-        "@": "/src",
-      },
+    vite: {
+        plugins: [tailwindcss()],
+        resolve: {
+            alias: {
+                '@': '/src',
+            },
+        },
     },
-  },
 });
